5214c00b679974446a095543d6a505a246622c32
rydowd <rydowd@gmail.com>
2020-07-01 14:10:27 -0400

    initial commit

---
+++ app/schema.py
@@ -0,0 +1,26 @@
+from marshmallow import Schema, fields
+
+class BrewSchema(Schema):
+  class Meta:
+    unknown = EXCLUDE
+  
+  brew_id = fields.Int(required=True)
+  author = fields.String()
+  author_id = fields.Email(required=True)
+  fermentables = fields.Dict()
+  hops = fields.Dict()
+  miscs = fields.Dict()
+  yeasts = fields.Dict()
+  waters = fields.Dict()
+  mash_steps = fields.Dict()
+  fermentation_steps = fields.Dict()
+  batch_size = fields.String()
+
+class ReminderSchema(Schema):
+  pass
+
+class UserSchema(Schema):
+  id = fields.Int(required=True)
+  user_id = fields.Email(required=True)
+  favorites = fields.List(fields.Int())
+
---
+++ parse_hops.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+def transform(hop):
+  return{
+    'name':hop['name'],
+    'alpha':hop['avgaa'],
+    'description':hop['description'],
+    'brand':hop['brand']
+  }
+
+output=[]
+
+with open('hops.json', 'r+') as data:
+  hops = json.load(data)
+
+  output = [transform(hop) for hop in hops if hop['brand']]
+
+print(output)
+with open ('parsed_hops.json', 'w') as f:
+  json.dump(output, f, indent=2)
